2135c28ad802f517cc85658e1a64d04fd1944740
Neo <neo@tiefkuehler-madness.me>
2020-11-21 02:10:09 +0100

    updated base

--- Class/base.py
+++ Class/base.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from selenium import webdriver
 from fake_useragent import UserAgent
-import time
+from pathlib import Path
+from random import randint
+import time, json, re, os
 
 class Base:
     def __init__(self):
@@ -24,11 +26,48 @@ class Base:
 
     def fetch(self,url):
         browser.get(url)
-        time.sleep(7)
+        time.sleep(randint(7,30))
         return browser.page_source
 
     def let(self):
+        currentOffers,count,src = "",1,"https://www.lowendtalk.com/categories/offers/p"
+        dataDir = os.getcwd()+"/data/lowendtalk/"
+
         print("Checking Lowendtalk")
         response = self.fetch("https://lowendtalk.com")
-        print(response)
+        if "Cloudflare" in response:
+            print("Failed to bypass CF")
+            browser.close()
+
+        while True:
+
+            print("Getting Offers")
+            response = self.fetch(src+str(str(count)+".json"))
+            rawJson = re.sub('<[^<]+?>', '', response)
+            try:
+                currentOffers = json.loads(rawJson)
+            except ValueError as e:
+                print("Failed to parse json")
+                browser.close()
+
+            if "Code" in currentOffers:
+                print("End of line")
+                browser.close()
+
+            print("Checking Offers")
+            if not os.path.exists(dataDir):
+                os.makedirs(dataDir)
+            for discussion in currentOffers['Discussions']:
+                file = dataDir+str(discussion['DiscussionID'])+"-"+discussion['FirstName']+".json"
+                print("Checking",discussion['DiscussionID'])
+                if not Path(file).is_file():
+                    data = {'id':discussion['DiscussionID'],'date':discussion['DateInserted'],'user':discussion['FirstName'],'post':discussion['Body']}
+                    with open(file, 'w') as f:
+                        json.dump(data, f)
+                else:
+                    print(discussion['DiscussionID'],"already exist")
+                    return True
+
+            count = count +1
+
         browser.close()
