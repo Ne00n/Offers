15e6710ee23adc63e8d3f4ba7f5c443d40d480d9
Neo <neo@tiefkuehler-madness.me>
2020-11-24 02:18:51 +0100

    updated data new structure

--- Class/data.py
+++ Class/data.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import time, json, re, os
+import time, pprint, json, re, os
 
 class Data:
     def __init__(self):
         print("Generate")
 
     def getProviders(self,cat,site):
-        dataDir = os.getcwd()+"/data/"+site+"/"+cat+"/"
+        dataDir = os.getcwd()+"/src/"+site+"/"+cat+"/"
         files = os.listdir(dataDir)
         providers = []
         for file in files:
@@ -18,19 +18,23 @@ class Data:
 
     def saveProviders(self,site,providers):
         providers.sort()
-        with open(os.getcwd()+"/data/"+site+"/providers.json", 'w') as f:
+        with open(os.getcwd()+"/data/"+site+"-providers.json", 'w') as f:
             json.dump(providers, f, indent=2)
 
+    def filterUrls(self,urls):
+        return urls
+
     def getUrls(self,cat,site):
-        dataDir = os.getcwd()+"/data/"+site+"/"+cat+"/"
+        dataDir = os.getcwd()+"/src/"+site+"/"+cat+"/"
         files = os.listdir(dataDir)
         data = []
         for file in files:
             with open(dataDir+file, 'r') as f:
                 post = json.load(f)
-            urls = re.findall("(https?:\/\/[A-Za-z0-9.\/?=&;_-]*)",post['post'], re.MULTILINE | re.DOTALL)
+            urlsRaw = re.findall("(https?:\/\/[A-Za-z0-9.\/?=&;_-]*)",post['post'], re.MULTILINE | re.DOTALL)
+            urls = self.filterUrls(urlsRaw)
             urls = list(set(urls))
             data.append({'id':post['id'],'post':{'date':post['date'],'urls':urls}})
         data = sorted(data, key=lambda k: k['id'],  reverse=True)
-        with open(os.getcwd()+"/data/"+site+"/urls-"+cat+".json", 'w') as f:
+        with open(os.getcwd()+"/data/"+site+"-urls-"+cat+".json", 'w') as f:
             json.dump(data, f, indent=2)
