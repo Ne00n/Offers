d84f45d06ff6d02398d01dd398db98215a33b072
Neo <neo@tiefkuehler-madness.me>
2020-12-02 13:25:47 +0100

    updated droplist

--- Class/data.py
+++ Class/data.py
@@ -30,7 +30,8 @@ class Data:
 
     def filterUrls(self,url):
         drop = ['twitter.com','facebook.com','imgur.com','github.com','speedtest.net','he.net','google.com','trustpilot.com','arin.net','webhostingtalk.com','discord.gg',
-        'youtube.com','tenor.com','instagram.com','githubusercontent.com','secure.gravatar.com','linkedin.com','lowendbox.com/wp-content/','lowendbox.com/tag/']
+        'youtube.com','tenor.com','instagram.com','githubusercontent.com','secure.gravatar.com','linkedin.com','lowendbox.com/wp-content/','lowendbox.com/tag/','goo.gl',
+        'cachefly.net']
         if any(domain in url for domain in drop):
             return True
         return False
